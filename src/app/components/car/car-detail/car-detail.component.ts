eedd84c4b27996ef634de5f646cd0810d75029fc
Gökhan SÖNMEZ <gsonmez828@aof.anadolu.edu.tr>
2021-03-17 19:41:38 +0300

    17.03 v2

--- src/app/components/car/car-detail/car-detail.component.ts
+++ src/app/components/car/car-detail/car-detail.component.ts
@@ -6,40 +6,51 @@ import { NgxGalleryOptions } from '@kolkov/ngx-gallery';
 import { NgxGalleryImage } from '@kolkov/ngx-gallery';
 import { NgxGalleryAnimation } from '@kolkov/ngx-gallery';
 import { environment } from 'src/environments/environment';
+import { CarImageService } from 'src/app/services/car-image.service';
+import { CarImage } from 'src/app/models/carImage';
 @Component({
   selector: 'app-car-detail',
   templateUrl: './car-detail.component.html',
   styleUrls: ['./car-detail.component.css'],
 })
 export class CarDetailComponent implements OnInit {
-  carsDetail: CarDetail[] = [];
+  cars: CarDetail[] = [];
+  images: CarImage[] = [];
   imageUrl = environment.baseUrl;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
   constructor(
     private carService: CarService,
+    private carImageService: CarImageService,
     private activetedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     this.activetedRoute.params.subscribe((params) => {
       this.getCarDetailById(params['carId']);
+      this.getCarImageByCarId(params['carId']);
     });
   }
   getCarDetailById(carId: number) {
     this.carService.getCarDetailById(carId).subscribe((response) => {
-      this.carsDetail = response.data;
+      this.cars = response.data;
+    });
+  }
+
+  getCarImageByCarId(carId: number) {
+    this.carImageService.getCarImageByCarId(carId).subscribe((response) => {
+      this.images = response.data;
       this.setGallery();
     });
   }
 
   getImages() {
     const imageUrls = [];
-    for (let i = 0; i < this.carsDetail.length; i++) {
+    for (let i = 0; i < this.images.length; i++) {
       imageUrls.push({
-        small: this.imageUrl + this.carsDetail[i].imagePath,
-        medium: this.imageUrl + this.carsDetail[i].imagePath,
-        big: this.imageUrl + this.carsDetail[i].imagePath,
+        small: this.imageUrl + this.images[i].imagePath,
+        medium: this.imageUrl + this.images[i].imagePath,
+        big: this.imageUrl + this.images[i].imagePath,
       });
     }
     return imageUrls;
